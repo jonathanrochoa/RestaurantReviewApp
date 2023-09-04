442f1ad02d3d47d7affed10a48e8aaf2671c3ec6
Jonathan Ochoa <ochoajonathan@outlook.com>
2023-09-04 17:27:21 -0400

    Added Db Data Context to project

---
+++ RestaurantReviewApp/Data/DataContext.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestaurantReviewApp.Models;
+
+namespace RestaurantReviewApp.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options)
+        { 
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Location> Locations { get; set; }  
+
+        public DbSet<Menu> Menus { get; set; }
+
+        public DbSet<Restaurant> Restaurants { get; set; }  
+
+        public DbSet<RestaurantCategory> RestaurantCategories { get; set;  }
+
+        public DbSet<Review> Reviews { get; set; }
+
+        public DbSet<Reviewer> Reviewers { get; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<RestaurantCategory>()
+                .HasKey(rc => new { rc.RestaurantId, rc.CategoryId });
+            modelBuilder.Entity<RestaurantCategory>()
+                .HasOne(r => r.Restaurant)
+                .WithMany(rc => rc.RestaurantCategories)
+                .HasForeignKey(r => r.RestaurantId);
+            modelBuilder.Entity<RestaurantCategory>()
+                .HasOne(r => r.Category)
+                .WithMany(rc => rc.RestaurantCategories)
+                .HasForeignKey(c => c.CategoryId);
+        }
+    }
+}
--- RestaurantReviewApp/Models/Restaurant.cs
+++ RestaurantReviewApp/Models/Restaurant.cs
@@ -7,7 +7,6 @@
         public ICollection<Review> Reviews { get; set;}
         public ICollection<Location> Locations { get; set; }
         public ICollection<Menu> Menus { get; set; }
-        public Category Category { get; set; }
         public ICollection<RestaurantCategory> RestaurantCategories { get; set;}
     }
 }
